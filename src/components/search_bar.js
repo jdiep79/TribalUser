2901bddbace6634964818c618292cf1d46eb07f2
Jimmy Diep <jdiep79@gmail.com>
2017-02-18 23:23:51 -0800

    remove import react component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 const SearchBar = ({ term, onSearchInputChange }) => {
     return (
@@ -11,7 +11,7 @@ const SearchBar = ({ term, onSearchInputChange }) => {
         />
       </form>
     );
-}
+};
 
 SearchBar.propTypes = {
   term: PropTypes.string.isRequired,
