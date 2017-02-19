0334d631379613d3d89b7e8a09860e93d8dfd950
Jimmy Diep <jdiep79@gmail.com>
2017-02-19 02:53:05 -0800

    add sort name component

---
+++ src/components/sort_name_radio.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react';
+
+const SortNameRadio = ({ sort, onRadioInputChange  }) => {
+  return (
+    <div>
+      <h2>Sort By</h2>
+      <div>
+      <label className="custom-control custom-radio">
+        <input 
+          name="name" 
+          type="radio" 
+          className="custom-control-input" 
+          onChange={ onRadioInputChange }
+          value="first"
+          checked={ sort === "first"}
+        />
+        <span className="custom-control-indicator"></span>
+        <span className="custom-control-description">First Name</span>
+      </label>
+      <br />
+      <label className="custom-control custom-radio">
+         <input 
+          name="last" 
+          type="radio" 
+          className="custom-control-input" 
+          onChange={ onRadioInputChange }
+          value="last"
+          checked={ sort === "last"}
+        />
+        <span className="custom-control-indicator"></span>
+        <span className="custom-control-description">Last Name</span>
+      </label>
+      </div>
+    </div>
+  );
+};
+
+export default SortNameRadio;
+
+// <div>
+//         <input onChange={ onRadioInputChange } type="radio" value="first" name="name" checked={ sort === "first"}/>First Name<br />
+//         <input onChange={ onRadioInputChange } type="radio" value="last" name="name" checked={ sort === "last"}/>Last Name<br />
+//       </div>
