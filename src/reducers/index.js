0ef9db43fe923bf259c8a16a0c00ed860832b960
Jimmy Diep <jdiep79@gmail.com>
2017-02-19 02:54:14 -0800

    add sort name result to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { retrieveUsersReducer } from './retrieve_users_reducer';
 import { searchUsersReducer } from './search_users_reducer';
+import { sortUsersReducer } from './sort_users_reducer';
  
 const rootReducer = combineReducers({
   users: retrieveUsersReducer,
-  term: searchUsersReducer
+  term: searchUsersReducer,
+  sort: sortUsersReducer
 });
 
 export default rootReducer;
