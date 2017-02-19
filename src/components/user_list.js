b4323f910ab76b05eb70dfb126818c3c6409ebc5
Jimmy Diep <jdiep79@gmail.com>
2017-02-18 21:38:43 -0800

    bootstrap styles and list users

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
-const UserList = () => {
+const UserList = (props) => {
+  const users = props.users.map((user) => {
+    return (
+       <div key={ user.cell } className="card">
+          <img className="card-img-top" src={ user.picture.large } alt="User Picture" />
+          <div className="card-block">
+            <h4 className="card-title">{ `${ user.name.first } ${ user.name.last }`}</h4>
+            <p className="card-text">{ `DOB: ${ user.dob }` }</p>
+            <p className="card-text">{ `City: ${ user.location.city  }` }</p>
+          </div>
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <img src="#" alt="User Picture" />
+    <div className="users-list">
+      { users }
     </div>
-  );
+  )
 }
 
 export default UserList;
