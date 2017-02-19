2c2e4eb5e6f9f644fbc72d7bd2219bddd7e4ff93
Jimmy Diep <jdiep79@gmail.com>
2017-02-18 17:11:59 -0800

    add user list component

---
+++ src/components/user_list.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const UserList = () => {
+  return (
+    <div>
+      <img src="#" alt="User Picture" />
+    </div>
+  );
+}
+
+export default UserList;
