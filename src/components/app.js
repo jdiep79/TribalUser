ac69a32e1d1d6bb3b22d4876978dc69d4b6446e3
Jimmy Diep <jdiep79@gmail.com>
2017-02-18 17:11:28 -0800

    add user section

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchSection from '../containers/search_section';
+import UserSection from '../containers/user_section';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchSection />
+        <UserSection />
       </div>
     );
   }
