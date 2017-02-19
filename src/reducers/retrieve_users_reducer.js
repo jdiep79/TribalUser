5aca972f46325ca287401e385b2b31b46b21b658
Jimmy Diep <jdiep79@gmail.com>
2017-02-18 23:26:26 -0800

    refactor export

--- src/reducers/retrieve_users_reducer.js
+++ src/reducers/retrieve_users_reducer.js
@@ -1,9 +1,11 @@
 import { RETRIEVE_USERS } from '../actions/retrieve_users_action';
 
-export const retrieveUsersReducer = (state = [], action) => {
+const retrieveUsersReducer = (state = [], action) => {
   if (action.type === RETRIEVE_USERS) {
     return [ ...action.payload.data.results, ...state ];
   }
 
   return state;
 }
+
+export { retrieveUsersReducer };
