8f4528717711fd6bbf9f61b9b833b349947fc7e0
Jimmy Diep <jdiep79@gmail.com>
2017-02-18 23:22:49 -0800

    refactor exports

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import SearchSection from '../containers/search_section';
 import UserSection from '../containers/user_section';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div>
@@ -12,3 +12,5 @@ export default class App extends Component {
     );
   }
 }
+
+export default App;
