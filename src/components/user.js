058628cf1609f74dc65ed3785e881e1997498951
Jimmy Diep <jdiep79@gmail.com>
2017-02-19 03:18:49 -0800

    allow group by last name

--- src/components/user.js
+++ src/components/user.js
@@ -1,11 +1,15 @@
 import React, { PropTypes } from 'react';
 
-const User = ({ user }) => {
+const User = ({ user, sort }) => {
+  const fullName = sort === 'first' ?
+    `${ user.name.first } ${ user.name.last }`:
+    `${ user.name.last }, ${ user.name.first }`
+
   return (
     <div className="card">
         <img className="card-img-top" src={ user.picture.large } alt="User Picture" />
         <div className="card-block">
-          <h4 className="card-title">{ `${ user.name.first } ${ user.name.last }`}</h4>
+          <h4 className="card-title">{ fullName }</h4>
           <p className="card-text">{ `DOB: ${ user.dob }` }</p>
           <p className="card-text">{ `City: ${ user.location.city  }` }</p>
         </div>
